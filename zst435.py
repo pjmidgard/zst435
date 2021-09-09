2e9ef2e7ccf234da3d9db9ddd67bbfcb27909c06
Java Software <pj.midgard@yahoo.com>
2021-09-09 11:48:34 +0100

    Add files via upload

--- zst435.py
+++ zst435.py
@@ -1989,7 +1989,7 @@ class compression:
                                     if Portal==8:
                                     	jl=b'\x00\x00\x00\x18\x66\x74\x79\x70\x6d\x70\x34'+jl
                                     if Portal==9:
-                                	jl=b'\xff\xd8\xff'+jl
+                                        jl=b'\xff\xd8\xff'+jl
                                     qqqwz=qqqwz+1
                                     szxzzza=""
                                     szxzs=""
