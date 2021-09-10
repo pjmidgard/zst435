f2b5363f125fcb1b6927fb0a1c804e28f3dd0973
Java Software <pj.midgard@yahoo.com>
2021-09-10 13:10:12 +0100

    Add files via upload

--- APIcompression.py
+++ APIcompression.py
@@ -4,7 +4,7 @@ from mtscomp import Writer, Reader
 # Define a writer to compress a flat raw binary file.
 w = Writer(chunk_duration=1.)
 # Open the file to compress.
-w.open('datab.bin', sample_rate=20000., n_channels=+1, dtype=np.int16)
+w.open('data.bin', sample_rate=20000., n_channels=+1, dtype=np.int16)
 # Compress it into a compressed binary file, and a JSON header file.
 w.write('data.cbin', 'data.ch')
 w.close()
